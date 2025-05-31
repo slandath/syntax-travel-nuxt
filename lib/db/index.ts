516f992edfaeaa57a0781997ffaffee759d0ff89
Tom Slanda <jesus_slanda@yahoo.com>
2025-05-31 11:36:06 -0400

    :sparkles: added auth (#55)
    * :sparkles: added auth
    
    Signed-off-by: Tom Slanda <jesus_slanda@yahoo.com>
    
    * updated lint.yaml with better auth secrets
    
    Signed-off-by: Tom Slanda <jesus_slanda@yahoo.com>
    
    ---------
    
    Signed-off-by: Tom Slanda <jesus_slanda@yahoo.com>

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
 
-import env from "~/lib/env";
-
+import env from "../env";
 import * as schema from "./schema";
-// You can specify any property from the libsql connection options
+
 const db = drizzle({
   connection: {
     url: env.TURSO_DATABASE_URL!,
