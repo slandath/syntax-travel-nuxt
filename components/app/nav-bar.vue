a8a14f0c3c1e2cff4b37035bd0c657686ffffbb9
slandath <jesus_slanda@yahoo.com>
2025-11-08 16:23:37 -0500

    added github signin
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -7,9 +7,7 @@
     </div>
     <div class="navbar-end">
       <AppThemeToggle />
-      <a class="btn btn-accent">Sign In with GitHub
-        <Icon name="tabler:brand-github" size="24" />
-      </a>
+      <AuthButton />
     </div>
   </div>
 </template>
---
+++ components/auth-button.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign In with GitHub
+    <span v-if="authStore.loading" class="loading loading-sppinner loading-md" />
+    <Icon name="tabler:brand-github" size="24" />
+  </button>
+</template>
